d7293474676db31174e44d298cc1eb638b007942
kskalisa <126727349+kskalisa@users.noreply.github.com>
2025-05-24 22:59:27 +0200

    Delete Models directory

--- Models/DTOs/OtpVerifyRequest.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ExamFinal.Models.DTOs;
-
-public class OtpVerifyRequest
-{
-    public string Email { get; set; } = null!;
-    public string Otp { get; set; } = null!;
-}
